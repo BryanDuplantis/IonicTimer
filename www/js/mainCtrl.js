5b2df21a47ecab436cc97e1ba3db4514ceade992
Bryan Duplantis <duplantis@gmail.com>
2015-07-09 14:15:59 -0500

    new changes

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -4,11 +4,25 @@ angular.module("starter")
 
     $scope.display = 0;
     $scope.startTime = 10;
+    // var intervalCtrl = $interval(function() {
+    //     $scope.display = $scope.startTime--;
+    //   }, 1000);
 
     $scope.startBtn = function () {
-      $interval(function() {
+      $interval(function () {
         $scope.display = $scope.startTime--;
       }, 1000);
+    }
+
+    $scope.stopInterval = function () {
+    //   if ($scope.startTime === 0) {
+    //     $interval.cancel();
+    //     stop = undefined;
+    //   }
+    }
+
+    $scope.stopBtn = function () {
+      $interval.cancel($interval);
     }
 
 }]);
