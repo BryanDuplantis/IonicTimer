8566deb24d930a54f9ea95b9a1b159d16d850079
Bryan Duplantis <duplantis@gmail.com>
2015-07-09 13:42:30 -0500

    added start timer function

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -1,9 +1,14 @@
 angular.module("starter")
 
-  .controller("mainCtrl", ['$scope', function($scope) {
+  .controller("mainCtrl", ['$scope', '$interval', function($scope, $interval) {
 
     $scope.display = 0;
-    $scope.startTime = "";
+    $scope.startTime = 10;
 
+    $scope.startBtn = function () {
+      $interval(function() {
+        $scope.display = $scope.startTime--;
+      }, 1000);
+    }
 
 }]);
